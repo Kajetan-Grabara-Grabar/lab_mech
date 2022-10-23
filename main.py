b86d260382d4a765ad9247298367b2d4a4123efc
kajetan-grabara-grabar <kajetan.grabara@gmail.com>
2022-10-23 13:29:00 +0200

    Pierwszt wykres z formatowaniem

--- main.py
+++ main.py
@@ -2,25 +2,16 @@ import matplotlib
 import matplotlib.pyplot as plt
 import pandas as pd
 import csv
-def import_data():
-    with open('data.csv', newline='', encoding='UTF-8') as f:
-        reader = csv.reader(f)
-        data = list(reader)
-    return data
 def wykres():
     data = pd.read_csv("data.csv",encoding="UTF-8")
     print(data)
-    plt.plot(data['Hz'],data['Obroty/minuty'],data['Prądnica Tachometryczna'])
-    plt.show()
-def test():
-    df = pd.read_csv(r'https://analityk.edu.pl/wp-content/uploads/2020/12/data.csv')
-    df['date'] = pd.to_datetime(df.date, format='%d/%m/%Y')
-    x=df['date']
-    y=df['Close']
-    plt.plot(x,y)
-    plt.show()
+    plt.plot(data['Hz'],data['Obroty/minuty'])
+    plt.title("Obroty na minutę względem Hz")
+    plt.xlabel("Hz")
+    plt.xticks(range(10, 31))
+    plt.grid(True)
+    plt.ylabel('Obroty na minutę')
+    plt.savefig('wyres1.png',dpi=300)
 
 if __name__=="__main__":
-    # plt.plot(import_data())
-    # test()
     wykres()
